c8a8677692432f223e57378fd70272d06fde3acc
Liz Martins <elizabeth.martins5@gmail.com>
2023-06-17 14:14:46 -0400

    progress save, styling nav bar, functionality to follow

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './index.css'
 import NavBar from './components/NavBar'
 
 const App = () => {
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 const NavBar = () => {
-  return <div>NavBar</div>
+  const links = [
+    { id: 1, link: 'home' },
+    { id: 2, link: 'about' },
+    { id: 3, link: 'portfolio' },
+    { id: 4, link: 'experience' },
+    { id: 5, link: 'contact' }
+  ]
+  return (
+    <div className="flex justify-between items-center w-full h-20 px-4 bg-black text-white fixed">
+      <div>
+        <h1 className="text-5xl font-signature ml-2">Liz</h1>
+      </div>
+      <ul className="hidden md:flex">
+        {links.map(({ id, link }) => (
+          <li
+            key={id}
+            className="px-5 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+          >
+            {link}
+          </li>
+        ))}
+      </ul>
+      {/* <div>{nav ? <FaBars size={30} /> : <FaTimes />}</div> */}
+    </div>
+  )
 }
 
 export default NavBar
