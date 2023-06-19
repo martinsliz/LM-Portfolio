ad145d9b76007e5dbbc4866416bab51c2644bf6e
Liz Martins <elizabeth.martins5@gmail.com>
2023-06-19 13:18:39 -0400

    rough nav bar completion

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useState } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
 
 const NavBar = () => {
+  const [nav, setNav] = useState(false)
   const links = [
     { id: 1, link: 'home' },
     { id: 2, link: 'about' },
@@ -18,13 +20,33 @@ const NavBar = () => {
         {links.map(({ id, link }) => (
           <li
             key={id}
-            className="px-5 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+            className="px-5 cursor-pointer capitalize font-medium text-purple-500 hover:scale-105 duration-200"
           >
             {link}
           </li>
         ))}
       </ul>
-      {/* <div>{nav ? <FaBars size={30} /> : <FaTimes />}</div> */}
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-700 text-gray-500">
+          <li>
+            {links.map(({ id, link }) => (
+              <li
+                key={id}
+                className="px-4 cursor-pointer capitalize py-6 text-4xl"
+              >
+                {link}
+              </li>
+            ))}
+          </li>
+        </ul>
+      )}
     </div>
   )
 }
