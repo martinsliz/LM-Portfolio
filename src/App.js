2e36efff1b22626bba333e4f94b26429693edc34
Liz Martins <elizabeth.martins5@gmail.com>
2023-06-19 13:19:50 -0400

    established home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './index.css'
 import NavBar from './components/NavBar'
+import Home from './components/Home'
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Home />
     </div>
   )
 }
