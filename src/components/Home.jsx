8b5d1e2751b01e7357a49891248248d007dc4c87
Liz Martins <elizabeth.martins5@gmail.com>
2023-06-20 11:15:33 -0400

    additional styling including adding color gradient to background and color gradient to button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,14 +6,14 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b from-black to-gray-800"
+      className="h-screen w-full bg-gradient-to-b from-black via-gray to-gray-800"
     >
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 text-gray-50">
-        <div>
-          <h2>Hello!</h2>
-          <p>Nice to meet you!</p>
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 text-gray-50 md:flex-row ">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">Hello!</h2>
+          <p className="text-gray-500 py-4 max-w-md">Nice to meet you!</p>
           <div>
-            <button>
+            <button className="text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-l from-cyan-200 to-blue-400 ">
               Portfolio
               <span>
                 <FaArrowRight />
