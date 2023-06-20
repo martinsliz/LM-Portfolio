e20980c91d8929735d0f27eb4e89ccfe0aa74cd1
Liz Martins <elizabeth.martins5@gmail.com>
2023-06-20 13:11:34 -0400

    added and tested functionality for social links

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './index.css'
 import NavBar from './components/NavBar'
 import Home from './components/Home'
+import Socials from './components/Socials'
 
 const App = () => {
   return (
     <div>
       <NavBar />
       <Home />
+      <Socials />
     </div>
   )
 }
--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,7 +1,76 @@
 import React from 'react'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
 
 const Socials = () => {
-  return <div>Socials</div>
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          LinkedIn <FaLinkedin size={30} />
+        </>
+      ),
+      href: 'https://www.linkedin.com/in/elizmartins/',
+      style: 'rounded-tr-md'
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          Github <FaGithub size={30} />
+        </>
+      ),
+      href: 'https://github.com/martinsliz'
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          Mail <HiOutlineMail size={30} />
+        </>
+      ),
+      href: 'mailto:elizabeth.martins5@gmail.com',
+      style: 'rounded-tr-md'
+    },
+    {
+      id: 4,
+      child: (
+        <>
+          Resume <BsFillPersonLinesFill size={30} />
+        </>
+      ),
+      href: '/resume.pdf',
+      style: 'rounded-br-md',
+      download: true
+    }
+  ]
+  return (
+    <div className="flex flex-col top-[35%] left-0 fixed ">
+      <ul>
+        {links.map(({ id, child, href, style, download }) => (
+          <li
+            key={id}
+            className={
+              'flex justify-between items-center w-40 h-14 px-4 ml-3 bg-slate-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300' +
+              '' +
+              'style'
+            }
+          >
+            <a
+              href={href}
+              className="flex justify-between items-center w-full text-white"
+              download={download}
+              target="_blank"
+            >
+              {child}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default Socials
