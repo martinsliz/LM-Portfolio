30125186ab541a4a433baaf264121395bf20be5e
Liz Martins <elizabeth.martins5@gmail.com>
2023-06-20 11:23:28 -0400

    created component Socials to direct to external links

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,12 +11,12 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 text-gray-50 md:flex-row ">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">Hello!</h2>
-          <p className="text-gray-500 py-4 max-w-md">Nice to meet you!</p>
+          <p className="text-gray-400 py-4 max-w-md">Nice to meet you!</p>
           <div>
-            <button className="text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-l from-cyan-200 to-blue-400 ">
+            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-l from-cyan-200 to-blue-400 cursor-pointer">
               Portfolio
-              <span>
-                <FaArrowRight />
+              <span className="group-hover:rotate-90 duration-300">
+                <FaArrowRight size={15} className="ml-3" />
               </span>
             </button>
           </div>
