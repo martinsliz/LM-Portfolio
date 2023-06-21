71d4d682a25a9124a7e640aaa825bbf50f72a331
Liz Martins <elizabeth.martins5@gmail.com>
2023-06-21 16:06:40 -0400

    rough in Portfolio page

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Experience = () => {
-  return <div>Experience</div>
+  return (
+    <div className="flex flex-col justify-center text-white bg-gradient-to-r from-black to-slate-600">
+      Experience
+    </div>
+  )
 }
 
 export default Experience
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
-// import arrayDestruct from '..assets/portfolio/arrayDestruct.jpg'
+import arrayDestruct from '../assets/portfolio/arrayDestruct.jpg'
+import installNode from '../assets/portfolio/installNode.jpg'
+import navbar from '../assets/portfolio/navbar.jpg'
+import reactParallax from '../assets/portfolio/reactParallax.jpg'
+import reactSmooth from '../assets/portfolio/reactSmooth.jpg'
+import reactWeather from '../assets/portfolio/reactWeather.jpg'
+import usestate from '../assets/portfolio/usestate.jpg'
 
 const Portfolio = () => {
   return (
-    <div>
-      <div className="flex flex-col justify-center text-white bg-gradient-to-r from-black to-slate-600">
-        PORTFOLIO
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4">Portfolio</p>
+          <p className="py-6">Check out my work here!</p>
+        </div>
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          <div className="shadow-md shadow-gray-600 rounded-lg">
+            <img
+              src={reactParallax}
+              alt=""
+              className="rounded-md duration-200 hover:scale-105 cursor-pointer"
+            />
+            <div className="flex items-center justify-center">
+              <button>Demo</button>
+              <button>Code</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
