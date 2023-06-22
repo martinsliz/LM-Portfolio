eec1dbd44e3f24b32f2e2017191922cc5a462b76
Liz Martins <elizabeth.martins5@gmail.com>
2023-06-21 21:57:19 -0400

    grid and styling on Experience component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -13,42 +13,49 @@ const Experience = () => {
       id: 1,
       src: html,
       name: 'HTML5',
-      href: ''
+      href: '',
+      style: 'shadow-orange-500'
     },
     {
       id: 2,
       src: javascript,
       name: 'JavaScript',
+      style: 'shadow-yellow-500',
       href: ''
     },
     {
       id: 3,
       src: css,
       name: 'CSS',
+      style: 'shadow-blue-500',
       href: ''
     },
     {
       id: 4,
       src: reactImage,
       name: 'React',
+      style: 'shadow-cyan-600',
       href: ''
     },
     {
       id: 5,
       src: node,
       name: 'Nodejs',
+      style: 'shadow-green-500',
       href: ''
     },
     {
       id: 6,
       src: github,
       name: 'Github',
+      style: 'shadow-gray-500',
       href: ''
     },
     {
       id: 7,
       src: tailwind,
       name: 'Tailwind CSS',
+      style: 'shadow-sky-500',
       href: ''
     }
   ]
@@ -65,10 +72,10 @@ const Experience = () => {
           <p className="py-6">Technologies</p>
         </div>
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
-          {programs.map(({ id, src, name }) => (
+          {programs.map(({ id, src, name, style }) => (
             <div
               key={id}
-              className="shadow-md hover:scale-105 duration-500 py2 rounded-lg"
+              className={`shadow-md hover:scale-105 duration-500 py2 rounded-lg ${style}`}
             >
               <img src={src} alt={name} className="w-20 mx-auto" />
               <p className="mt-4">{name}</p>
