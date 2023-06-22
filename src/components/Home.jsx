8b838b0d583c1691272809d910ac087992548043
Liz Martins <elizabeth.martins5@gmail.com>
2023-06-22 15:20:02 -0400

    added smooth scrolling to portfolio button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-scroll'
 import heroImage from '../assets/heroImage.png'
 import { FaArrowRight } from 'react-icons/fa'
 
@@ -13,18 +14,23 @@ const Home = () => {
           <h2 className="text-4xl sm:text-7xl font-bold text-white">Hello!</h2>
           <p className="text-gray-400 py-4 max-w-md">Nice to meet you!</p>
           <div>
-            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-l from-cyan-200 to-blue-400 cursor-pointer">
+            <Link
+              to="portfolio"
+              smooth
+              duration={800}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-l from-cyan-200 to-blue-400 cursor-pointer"
+            >
               Portfolio
               <span className="group-hover:rotate-90 duration-300">
                 <FaArrowRight size={15} className="ml-3" />
               </span>
-            </button>
+            </Link>
           </div>
         </div>
         <div>
           <img
             src={heroImage}
-            alt="owner headshot"
+            alt="main subject"
             className="rounded-2xl mx-auto w-2/3 md:w-full"
           />
         </div>
