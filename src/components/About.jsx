591011a83e816d5097d872d6eb7a2a72e24fae79
Liz Martins <elizabeth.martins5@gmail.com>
2023-06-29 13:29:09 -0400

    adjusted profile photo and added proper text to the About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,21 +9,31 @@ const About = () => {
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-            About
+            A little about me:
           </p>
         </div>
         <p className="text-xl mt-20">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero,
-          assumenda enim delectus dignissimos nobis veniam adipisci suscipit ut
-          cupiditate natus inventore fuga sit, repellat debitis quibusdam.
-          Laborum mollitia sit commodi.
+          With a diverse career in industries such as construction and customer
+          service, I bring a unique perspective to the world of tech. In my most
+          recent role as an assistant project manager, I contributed to the
+          success of commercial construction projects and hope to carry that
+          through to my next venture. Beyond my professional experience, I have
+          a background in theater and also speak three languages. This has
+          helped me to develop strong strong interpersonal skills and an ability
+          to collaborate and communicate with people from diverse backgrounds. I
+          bring a positive attitude and great sense of humor to every team I
+          work with.
         </p>
         <br />
         <p className="text-xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-          possimus sapiente, porro ratione eveniet reiciendis distinctio libero
-          eum labore dolorem delectus repudiandae provident quia voluptatem
-          voluptate quaerat itaque facere saepe.
+          I have a natural curiosity and love of tinkering and problem-solving.
+          Whether it's repairing an oven control board, building a pc during a
+          pandemic, or restoring a vintage Schwinn, I thrive on finding creative
+          solutions to complex problems. Ultimately, I am seeking a
+          collaborative work environment where I can bring my skills,
+          experience, and enthusiasm to the table. I am excited to work with
+          people who value interaction, communication, and creativity, and I am
+          committed to bringing my best to every project I work on.
         </p>
       </div>
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-scroll'
-import heroImage from '../assets/heroImage.png'
+import LM from '../assets/LM.jpeg'
 import { FaArrowRight } from 'react-icons/fa'
 
 const Home = () => {
@@ -12,7 +12,9 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 text-gray-50 md:flex-row ">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">Hello!</h2>
-          <p className="text-gray-400 py-4 max-w-md">Nice to meet you!</p>
+          <p className="text-gray-400 text-lg py-4 max-w-md">
+            Check out my work below!
+          </p>
           <div>
             <Link
               to="portfolio"
@@ -29,9 +31,9 @@ const Home = () => {
         </div>
         <div>
           <img
-            src={heroImage}
+            src={LM}
             alt="main subject"
-            className="rounded-2xl mx-auto w-2/3 md:w-full"
+            className="rounded-full max-w-screen-sm max-h-96 md:w-full"
           />
         </div>
       </div>
